cbd4510c55275df74301bcd9232bc2733d03c456
Fallen <amir.amon@mail.ru>
2021-10-01 10:56:59 +0900

    New UI

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,12 +3,21 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+
+    private TextView showTxt;
+    private TextView resTxt;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private void init() {
+        showTxt = (TextView) findViewById(R.id.inputTxt);
+        resTxt = (TextView) findViewById(R.id.resTxt);
     }
 }
